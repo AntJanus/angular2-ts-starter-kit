a826dc23d6b4b41725ca61944f3a45799f316ad1
Antonin Januska <antonin@antjanus.com>
2015-12-15 21:28:01 -0700

    Betafied

---
+++ src/app/boot.ts
@@ -0,0 +1,8 @@
+/// <reference path="../typings/tsd.d.ts" />
+
+import 'reflect-metadata';
+
+import { bootstrap } from 'angular2/platform/browser';
+import { MyAppComponent } from './app.component';
+
+bootstrap(MyAppComponent);
--- src/app/components/subcomponent/subcomponent.component.ts
+++ src/app/components/subcomponent/subcomponent.component.ts
@@ -1,4 +1,4 @@
-import {Component, View} from 'angular2/angular2';
+import {Component, View} from 'angular2/core';
 
 import { MyService } from '../../services/sampleService';
 
